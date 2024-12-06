a3ac8b142340a8747a7dd299c19f102ba5a19383
karthi9772 <karthi.g9772@gmail.com>
2024-12-07 01:05:35 +0530

    minor change

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import NavBar from "./components/NavBar";
 import NewsBoard from "./components/NewsBoard";
 
 const App = () => {
-  const [category, setCategory] = useState("latest");
+  const [category, setCategory] = useState("general");
   return (
     <div>
       <NavBar setCategory={setCategory}></NavBar>
